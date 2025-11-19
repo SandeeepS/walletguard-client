6b178d01f5b23f311b24e489b393da13fd6afef3
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-11-19 17:24:41 +0530

    created a base

---
+++ src/pages/user/UserLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserLayout: React.FC = () => {
+  return (
+    <>
+      <div>{/**provide header herer */}</div>
+
+      <div>
+        
+      </div>
+    </>
+  );
+};
+
+export default UserLayout;
